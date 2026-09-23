efdbb375652bca91f8c9a19b7bd9ca3187d71e29
YaBa001 <venuscollective@icloud.com>
2026-09-23 14:36:36 -0400

    Fix audit key policy syntax

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -111,21 +111,30 @@ data "aws_iam_policy_document" "data_key" {
   statement {
     sid       = "EnableAccountAdministration"
     effect    = "Allow"
-    principals { type = "AWS" identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"] }
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
     actions   = ["kms:*"]
     resources = ["*"]
   }
   statement {
     sid       = "AllowCloudTrailEncryption"
     effect    = "Allow"
-    principals { type = "Service" identifiers = ["cloudtrail.amazonaws.com"] }
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
     actions   = ["kms:GenerateDataKey*", "kms:DescribeKey"]
     resources = ["*"]
   }
   statement {
     sid       = "AllowCloudWatchLogsEncryption"
     effect    = "Allow"
-    principals { type = "Service" identifiers = ["logs.${var.region}.amazonaws.com"] }
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${var.region}.amazonaws.com"]
+    }
     actions   = ["kms:Encrypt*", "kms:Decrypt*", "kms:ReEncrypt*", "kms:GenerateDataKey*", "kms:Describe*"]
     resources = ["*"]
   }
